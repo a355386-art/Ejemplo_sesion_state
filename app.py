2e7250823ad5116ea857fe8e5f60dbf19f088a8b
a355386-art <a355386@uach.mx>
2025-10-29 08:29:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ st.title("Ejemplo para usar session_state")
 if "count" not in st.session_state:
   st.session_state["count"] = 0
 
+if "name" not in st.session_state:
+  st.session_state["name"]=
+
 st.write(st.session_state)
 
 if st.button("click me"):
   st.session_state["count"] += 1
+
+nombre = st.text_import("Escribe tu nombre")
+st.write(nombre)
+
+st.write(st.session_state)
