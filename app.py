cc3678483506d21fcdd4f5d8ab7c512819ec3a24
a355386-art <a355386@uach.mx>
2025-10-29 08:14:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-import streamlit as at
+import streamlit as st
 st.title("Ejemplo para usar session_state")
 
 if "count" not in st.session_state:
   st.session_state["count"] = 0
 
 st.write(st.session_state)
+
+if st.button("click me"):
+  st.session_state["count"] += 1
