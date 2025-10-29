803a6b929595de809dbba1c53fab18874352f3fd
a355386-art <a355386@uach.mx>
2025-10-29 07:50:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as at
 st.title ("Ejemplo para usarpara usar session_state")
+
 count = 0
 
 increment = st.button("increment")
 if increment: 
   count += 1
-st.write("count = ",count)
+  
+st.write("count =", count)
